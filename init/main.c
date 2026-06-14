f013d58aa2fbbfd23fcd60cb0e9d707c3a8a896a
mizukanaAlt <rina301087@gmail.com>
2026-06-14 16:07:18 +0000

    init: main: unified fingerprint ysl

--- init/main.c
+++ init/main.c
@@ -555,14 +555,14 @@ static void __init mm_init(void)
 	pti_init();
 }
 
-#ifdef CONFIG_MACH_XIAOMI_YSL
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_YSL)
 int fpsensor=1;
 #endif
 asmlinkage __visible void __init start_kernel(void)
 {
 	char *command_line;
 	char *after_dashes;
-#ifdef CONFIG_MACH_XIAOMI_YSL
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_YSL)
 	char *p=NULL;
 #endif
 
@@ -594,12 +594,14 @@ asmlinkage __visible void __init start_kernel(void)
 	page_alloc_init();
 
 	pr_notice("Kernel command line: %s\n", boot_command_line);
-#ifdef CONFIG_MACH_XIAOMI_YSL
-	p = strstr(boot_command_line, "androidboot.fpsensor=fpc");
-	if (p)
-		fpsensor = 1; // fpc1020
-	else
-		fpsensor = 2; // gf3208
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_YSL)
+	if (xiaomi_msm8953_mach_get() == XIAOMI_MSM8953_MACH_YSL) {
+		char *p = strstr(boot_command_line, "androidboot.fpsensor=fpc");
+		if (p)
+			fpsensor = 1; // fpc1020
+		else
+			fpsensor = 2; // gf3208
+	}
 #endif
 	/* parameters may set static keys */
 	jump_label_init();
