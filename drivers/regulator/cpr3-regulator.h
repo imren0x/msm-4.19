6e1e5db3f8a709b625dbb293d8adb83e795bfed1
Mika <enashinonome264@gmail.com>
2026-07-17 05:42:51 +0000

    Revert "regulator: cpr4-apss: Update APC CPR fuse combo logic for msm8953"
    This reverts commit b5fbf923b67b8abdd97e37a09ad48df45fb952d2.

--- drivers/regulator/cpr3-regulator.h
+++ drivers/regulator/cpr3-regulator.h
@@ -914,8 +914,6 @@ void cprh_adjust_voltages_for_apm(struct cpr3_regulator *vreg);
 void cprh_adjust_voltages_for_mem_acc(struct cpr3_regulator *vreg);
 int cpr3_adjust_target_quotients(struct cpr3_regulator *vreg,
 			int *fuse_volt_adjust);
-int cpr3_parse_fuse_combo_map(struct cpr3_regulator *vreg, u64 *fuse_val,
-			int fuse_count);
 
 #else
 
@@ -1098,12 +1096,6 @@ static inline int cpr3_adjust_target_quotients(struct cpr3_regulator *vreg,
 			int *fuse_volt_adjust)
 {
 	return 0;
-}
-
-static int cpr3_parse_fuse_combo_map(struct cpr3_regulator *vreg, u64 *fuse_val,
-			int fuse_count)
-{
-	return -EPERM;
 }
 
 #endif /* CONFIG_REGULATOR_CPR3 */
